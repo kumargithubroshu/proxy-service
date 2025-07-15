a96e073f9e419af4a22082db9283012491dea68f
Roshan <Roshan@DESKTOP-61S9O1S.lan>
2025-07-15 14:21:38 -0700

    adding proxy service to handle gateway

---
+++ src/main/java/com/proxy/example/gateway_api/GatewayApiApplication.java
@@ -0,0 +1,13 @@
+package com.proxy.example.gateway_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApiApplication.class, args);
+	}
+
+}
